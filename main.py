cbcf21c535c624e9a5693b60fe00fd4ca3924bc6
yakup ışık <yakupsehmus@gmail.com>
2021-07-15 15:58:43 +0300

    This is a new commit for what I originally planned to be amended

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.mackolik.com/puan-durumu/t%C3%BCrkiye-s%C3%BCper-lig/2020-2021/482ofyysbdbeoxauk19yg7tdt"
+response = requests.get(url)
+
+html_icerigi = response.content
+soup = BeautifulSoup(html_icerigi, "html.parser")
+#print(soup)
+
+"""
+for i in soup.find_all("a"): #sitenin html sindeki tüm a etiketlerini gösterir
+    print(i.get("href")) #sitenin içinde girilebilcek linkleri gösterir eğer hrefe yerine text yazarsan linkin üstündeki isimleri görürsün
+    print("*************************************************")
+"""
+
+"""
+print(soup.find_all("div", {"class":"page-section-header"}))
+# classı .. olan divleri gösterir
+"""
+
+#for i in soup.find_all("tr"):
+#    print(i.get("data-team-name"))
+for i in soup.find_all("tr"):
+    print(i.text)
